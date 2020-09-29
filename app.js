ccd1fb6a19f2251258126733d7556f727a5bb09f
Rahul kumar <caltoonme2@gmail.com>
2020-09-29 15:52:36 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+let fruits = 'my name is rahul'
+
+let a = ['ram', 'shyam'];
+
+let b = ['rahul', 'ajay'];
+
+let c = ['sanju', 'sonu'];
+    
+
+
+// // ( m =  () => {
+// //    console.log('hudfklhdslfh')
+
+// // })()
+ 
+(() => console.log('AJAY'))()
